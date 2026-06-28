f2f772920a9ad971b9cbec92dfe3787759a54304
ivo <icorrale@student.42heilbronn.de>
2026-06-28 15:30:40 +0200

    ex0 almost finished

--- ex0/battle.py
+++ ex0/battle.py
@@ -4,23 +4,33 @@ from creatures import CreatureFactory, FlameFactory, AquaFactory
 def test_factory(fac: CreatureFactory):
     base_creature = fac.create_base()
     evolved_creature = fac.create_evolved()
-    base_creature.describe()
-    base_creature.attack()
-    evolved_creature.describe()
-    evolved_creature.attack()
+    print(base_creature.describe())
+    print(base_creature.attack())
+    print(evolved_creature.describe())
+    print(evolved_creature.attack())
 
 
 def fight(fac_one: CreatureFactory, fac_two: CreatureFactory):
-    pass
+    base_first = fac_one.create_base()
+    base_second = fac_two.create_base()
+    print(base_first.describe())
+    print("\tvs.")
+    print(base_second.describe())
+    print("fight!")
+    print(base_first.attack())
+    print(base_second.attack())
 
 
 def main() -> None:
     print("Testing factory")
     f_fac = FlameFactory()
     test_factory(f_fac)
+    print()
     print("Testing factory")
     a_fac = AquaFactory()
     test_factory(a_fac)
+    print("\nTesting battle")
+    fight(f_fac, a_fac)
 
 
 if __name__ == "__main__":
